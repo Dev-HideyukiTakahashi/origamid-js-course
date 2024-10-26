6da3bbb82cda19ec67aab35b42cc64abb3c4b891
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-10-26 04:07:24 -0300

    feat: accordion

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
 /* Navegacao por tabs */
 
-const tabMenu = document.querySelectorAll('.js-tab-menu li');
-const tabContent = document.querySelectorAll('.js-tab-content section');
+function initTabiNav() {
+  const tabMenu = document.querySelectorAll('.js-tab-menu li');
+  const tabContent = document.querySelectorAll('.js-tab-content section');
 
-// deixando a primeira sessão com class 'ativo' ao carregar página
-tabContent[0].classList.add('ativo');
+  // deixando a primeira sessão com class 'ativo' ao carregar página
+  tabContent[0].classList.add('ativo');
 
-// função callback
-function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove('ativo');
+  // função callback
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove('ativo');
+    });
+    tabContent[index].classList.add('ativo');
+  }
+
+  // ao clicar em determinada imagem traz a sessão respectiva
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+      activeTab(index);
+    });
   });
-  tabContent[index].classList.add('ativo');
 }
-
-// ao clicar em determinada imagem traz a sessão respectiva
-tabMenu.forEach((itemMenu, index) => {
-  itemMenu.addEventListener('click', () => {
-    activeTab(index);
-  });
-});
-
-/* Navegacao por tabs FIM*/
+/* Navegacao por tabs FIM */
 /* ------------------------------------------------------------------------------- */
 
+/* Accordion list */
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  accordionList[0].classList.add('ativo');
+  accordionList[0].nextElementSibling.classList.add('ativo');
+
+  function activeAccordion() {
+    this.classList.toggle('ativo');
+    this.nextElementSibling.classList.toggle('ativo');
+  }
+
+  accordionList.forEach(item => {
+    item.addEventListener('click', activeAccordion);
+  });
+}
+
+/* Accordion list FIM */
+/* ------------------------------------------------------------------------------- */
+
+
+/* INIT */
+initTabiNav();
+initAccordion();
