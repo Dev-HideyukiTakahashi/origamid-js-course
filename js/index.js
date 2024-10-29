3891af3f65687d21b97484454e15077744af00b6
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-10-29 02:58:56 -0300

    feat: modal

--- js/index.js
+++ js/index.js
@@ -2,8 +2,10 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimaScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/acordion.js";
 import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
 
 initScrollSuave();
 initAnimaScroll();
 initAccordion();
 initTabNav();
+initModal();
