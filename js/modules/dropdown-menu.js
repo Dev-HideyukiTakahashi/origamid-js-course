3df14dd9dc7915679d44e1aeb47c191667aa1df6
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-10-30 02:10:16 -0300

    fix: dropdown

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,40 +1,37 @@
 // o dropdown foi resolvido apenas com css
 // essa classe foi criada apenas para acessibilidade mobile
 export default function initDropdownmenu() {
+  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
-}
+  dropdownMenus.forEach(menu => {
+    menu.addEventListener('touchstart', handleClick);
+    menu.addEventListener('click', handleClick);
 
+  })
 
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-
-dropdownMenus.forEach(menu => {
-  menu.addEventListener('touchstart', handleClick);
-  menu.addEventListener('click', handleClick);
-
-})
-
-function handleClick(event) {
-  event.preventDefault();
-  this.classList.toggle('active');
-  outsideClick(this, () => {
-    this.classList.remove('active')
-  });
-}
-
-// para mobile, ao clicar fora do dropdown fechar
-function outsideClick(element, callback) {
-  const html = document.documentElement;
-  const outside = 'data-outside'
-
-  if (element.hasAttribute(outside)) {
-    html.addEventListener('click', handleOutsideClick);
-    element.setAttribute(outside, '')
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.toggle('active');
+    outsideClick(this, () => {
+      this.classList.remove('active')
+    });
   }
-  function handleOutsideClick(event) {
-    if (!element.contains(event.target)) {
-      element.removeAttribute(outside, '')
-      html.removeEventListener('click', handleOutsideClick);
-      callback();
+
+  // para mobile, ao clicar fora do dropdown fechar
+  function outsideClick(element, callback) {
+    const html = document.documentElement;
+    const outside = 'data-outside'
+
+    if (element.hasAttribute(outside)) {
+      html.addEventListener('click', handleOutsideClick);
+      element.setAttribute(outside, '')
+    }
+    function handleOutsideClick(event) {
+      if (!element.contains(event.target)) {
+        element.removeAttribute(outside, '')
+        html.removeEventListener('click', handleOutsideClick);
+        callback();
+      }
     }
   }
 }
